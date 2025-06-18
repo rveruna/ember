acbb1ac1e4bef50697464aa2358cdd6066b8c581
veronikar <veronika.rosicova@RD-VeronikaR2-Mac.local>
2025-06-18 15:03:19 +0100

    feat: add map container to TripPage with default view over Scotland

--- src/pages/TripPage.tsx
+++ src/pages/TripPage.tsx
@@ -1,8 +1,25 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 export const TripPage = () => {
   return (
     <div style={{ padding: "1rem" }}>
       <h1>Trip Page</h1>
       <p>This is where the trip data will appear.</p>
+
+      <section style={{ height: "240px", marginTop: "1rem" }}>
+        <MapContainer
+          center={[56.4907, -4.2026]} // center of Scotland
+          zoom={7}
+          scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            attribution="&copy; OpenStreetMap contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </section>
     </div>
   );
 };
