776e5754b8ebe0659acc10d16382c834dccec348
veronikar <veronika.rosicova@RD-VeronikaR2-Mac.local>
2025-06-18 16:44:36 +0100

    fix: format time to accept string and date

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,4 +1,4 @@
-export function formatTime(dateStr: string) {
-  const date = new Date(dateStr);
+export function formatTime(input: string | Date) {
+  const date = typeof input === "string" ? new Date(input) : input;
   return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
 }
