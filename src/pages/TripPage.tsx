c9f3f3968f13d26fc976ea189a0d6ca1af1198e4
veronikar <veronika.rosicova@RD-VeronikaR2-Mac.local>
2025-06-18 14:57:29 +0100

    feat: add simple trip page

---
+++ src/pages/TripPage.tsx
@@ -0,0 +1,8 @@
+export const TripPage = () => {
+  return (
+    <div style={{ padding: "1rem" }}>
+      <h1>Trip Page</h1>
+      <p>This is where the trip data will appear.</p>
+    </div>
+  );
+};
