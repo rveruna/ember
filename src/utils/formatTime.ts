f51ddaaecc005876a98425b792e02015dc6aa384
veronikar <veronika.rosicova@RD-VeronikaR2-Mac.local>
2025-06-19 07:24:06 +0100

    fix: remove AM/PM from time format util

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,4 +1,8 @@
 export function formatTime(input: string | Date) {
   const date = typeof input === "string" ? new Date(input) : input;
-  return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  return date.toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
 }
