02e9449bb85d9264d5d32654a20289c7092e5f12
veronikar <veronika.rosicova@RD-VeronikaR2-Mac.local>
2025-06-19 09:55:03 +0100

    fix: some more jest setup fixing fml

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,27 +1,14 @@
-import { pathsToModuleNameMapper } from "ts-jest";
-import { readFileSync } from "fs";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
-
-// Manual JSON read because `assert { type: "json" }` fails in older Node
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const tsconfig = JSON.parse(
-  readFileSync(join(__dirname, "tsconfig.json"), "utf-8")
-);
-
 export default {
-  preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
-  transform: {
-    "^.+\\.[tj]sx?$": ["ts-jest", { useESM: true }],
-  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+
+  transform: {},
   moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^@/(.*)$": "<rootDir>/src/$1",
-    "\\.module\\.css$": "identity-obj-proxy",
-    ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
-      prefix: "<rootDir>/",
-    }),
   },
+
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  preset: "ts-jest/presets/js-with-ts-esm",
 };
